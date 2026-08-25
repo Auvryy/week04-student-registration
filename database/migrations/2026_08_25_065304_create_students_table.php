36696040673a59642b3663f7d12213d0d29f774e
Andy <andy@example.com>
2026-08-25 14:59:33 +0800

    feat: create students migration

---
+++ database/migrations/2026_08_25_065304_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_id', 50)->unique();
+            $table->string('first_name', 100);
+            $table->string('middle_name', 100)->nullable();
+            $table->string('last_name', 100);
+            $table->string('email', 150)->unique();
+            $table->string('mobile_number', 20);
+            $table->string('gender', 20);
+            $table->date('date_of_birth');
+            $table->string('program', 100);
+            $table->string('year_level', 50);
+            $table->text('address');
+            $table->string('profile_picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
